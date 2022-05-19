3ac0e8a67f7d1c60b96427081b5d949edf3f213e
EvergSwe <daniel.nyberg@everg.se>
2022-05-19 20:00:23 +0000

    connecting start game functions to onclick button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,30 @@ let mole = document.getElementsByClassName("mole");
 let timeLeft = document.getElementById("timeleft");
 let score = document.getElementById("score");
 
+/**
+ * remove "old" mole before randomly placing a "new"
+ * //generate random integer between 0-8 and place mole
+ */
 function randomSquare() {
-   for (let i=0; i<squares.length; i++) { //remove "old" mole before randomly placing a "new"
+   for (let i=0; i<squares.length; i++) { 
    squares[i].classList.remove("mole");
    }
 
-   let randomSquare = squares[Math.floor(Math.random() * 9)]; //generate random number between 0-8
+   let randomSquare = squares[Math.floor(Math.random() * 9)]; 
    randomSquare.classList.add("mole");
    }
 
+// onclick easy game button, start easy game with interval 1s
 function startEasy() {
    let timer = null
    timer = setInterval(randomSquare, 1000)
    }
 
+// onclick hard game button, start hard game with interval 0.7s
 function startHard() {
    let timer = null
    timer = setInterval(randomSquare, 700)
    }
 
-   startHard()
 
    

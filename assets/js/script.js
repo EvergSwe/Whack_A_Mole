acd45a548abef5ed2060600ea474c22a7e87161e
EvergSwe <daniel.nyberg@everg.se>
2022-05-21 10:21:37 +0000

    timeleft bug fixed for restaring the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
-let squares = document.getElementsByClassName("square");
-let mole = document.getElementsByClassName("mole");
 let score = document.getElementById("score");
 
-let timer = null;
+//global 
+let timer = null; 
 
 /**
  * onclick easy-game button, start easy game with interval 1s
@@ -11,7 +10,6 @@ let timer = null;
 function startEasy() {   
    timer = setInterval(randomSquare, 1000);
    countDownSeconds();
-   randomSquare();
 }
 
 /**
@@ -25,20 +23,21 @@ function startHard() {
 
 /**
  * when starting the game the the clock countdown starts from 60
- * value reduce by 1 at an interval of 1s
- * when 
+ * time value reduce by 1 at an interval of 1s
+ * 
  * */
 function countDownSeconds() {
 
-   let time = document.getElementById("timeleft");
-   let countDownTimer = setInterval(function() {  
-   let newTime = time.innerHTML;
+   time = document.getElementById("timeleft");
+   countDownTimer = setInterval(function() {  
+   newTime = time.innerHTML;
    newTime--;
    time.innerHTML = newTime;
 
-   if (newTime === 0) {
+   if (newTime === -1) {
       clearInterval(countDownTimer);
-      clearInterval(timer);
+      clearInterval(timer)
+      time.innerHTML = 5; // change to 30 when finalizing
       alert("GAME OVER!");
       }
    }, 1000);
@@ -51,6 +50,10 @@ function countDownSeconds() {
  * get moleLocation square id
  */
 function randomSquare() {
+
+   let squares = document.getElementsByClassName("square");
+   let mole = document.getElementsByClassName("mole");
+
    for (let i=0; i<squares.length; i++) { 
    squares[i].classList.remove("mole");
    }
@@ -58,11 +61,10 @@ function randomSquare() {
    let randomSquare = squares[Math.floor(Math.random() * 9)]; 
    randomSquare.classList.add("mole");
 
-   moleLocation = randomSquare.id;
+   let moleLocation = randomSquare.id;
    console.log(moleLocation) //Remove when finalizing
 
-   //check mouse down location if position == moleLocation if it does result++ else result
-
+//check mouse down location if position == moleLocation if it does result++ else result
 
 }
 
